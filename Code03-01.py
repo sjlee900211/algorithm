f94fc24b6f93f260d76605ff71be5670c9c4d054
Celine Lee <sjlee900211@gmail.com>
2021-07-21 10:53:24 +0900

    first commit

---
+++ Code03-01.py
@@ -0,0 +1,22 @@
+## 함수
+
+
+## 전역변수
+katok = [] # 빈 배열
+
+
+## 메인코드
+katok.append(None) # 빈칸 1개 확보
+katok[0] = '다현'
+
+katok.append(None)
+katok[1] = '정연'
+
+katok.append(None)
+katok[2] = '쯔위'
+
+katok.append(None)
+katok[3] = '사나'
+
+katok.append(None)
+katok[4] = '지효'
