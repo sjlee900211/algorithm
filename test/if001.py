2513301d6119812bc30656d3dc49eb6023e141c6
Celine Lee <sjlee900211@gmail.com>
2021-07-23 15:45:54 +0900

    if 2nd

--- test/if001.py
+++ test/if001.py
@@ -3,11 +3,11 @@ if x >= 90 :
     print("Pass")
     
 value = [70, 80, 90, 95]
-
-for value in range(len(value)) :
-    if (value >= 90) :
-        print(value, "축하합니다.")
+i = -1
+for i in range(len(value)) :
+    if (value[i] >= 90) :
+        print(value[i], "축하합니다.")
         print("당신은 합격입니다.")
     else :
-        print(value,"죄송합니다.")
+        print(value[i],"죄송합니다.")
         print("당신은 불합격입니다.")
